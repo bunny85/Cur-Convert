f1ee657dfc28b01b83342c1d1f0bac7c0a93041c
bunny85 <b.rompicharla@gmail.com>
2017-09-12 00:27:43 -0400

    all

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+function rupeeConverter(){
+document.converter.euro.value = document.converter.rupee.value * 0.014	
+document.converter.dollar.value = document.converter.rupee.value * 0.016
+document.converter.pound.value = document.converter.rupee.value * 0.012
+document.converter.yen.value = document.converter.rupee.value * 1.74
+}
+function euroConverter(){
+document.converter.dollar.value = document.converter.euro.value * 1.470
+document.converter.pound.value = document.converter.euro.value * 0.717
+document.converter.yen.value = document.converter.euro.value * 165.192
+document.converter.rupee.value = document.converter.euro.value * 72.03	
+}
+function dollarConverter(){
+document.converter.euro.value = document.converter.dollar.value * 0.680
+document.converter.pound.value = document.converter.dollar.value * 0.488
+document.converter.yen.value = document.converter.dollar.value * 112.36
+document.converter.rupee.value = document.converter.dollar.value * 64.43
+}
+function poundConverter(){
+document.converter.dollar.value = document.converter.pound.value * 2.049
+document.converter.euro.value = document.converter.pound.value * 1.394
+document.converter.yen.value = document.converter.pound.value * 230.27
+document.converter.rupee.value = document.converter.pound.value * 81.95
+}
+function yenConverter(){
+document.converter.dollar.value = document.converter.yen.value * 0.0089
+document.converter.pound.value = document.converter.yen.value * 0.00434
+document.converter.euro.value = document.converter.yen.value * 0.00605
+document.converter.rupee.value = document.converter.yen.value * 0.58
+}
